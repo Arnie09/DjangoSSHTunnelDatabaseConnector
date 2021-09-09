4be9d0b583b8b59c6639942227306c89314583f3
arnab0998 <arnab.chanda@thoughtworks.com>
2021-09-09 20:32:03 +0530

    Removed logging from dependency

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from distutils.core import setup
 setup(
     name='DjangoSSHTunnelDatabaseConnector',
     packages=['DjangoSSHTunnelDatabaseConnector'],
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='This is a library that enables one to use django models to do CRUD operations on a database ' +
                 'hosted remotely and accessible through ssh in a pythonic way',
     author='Arnab Chanda',
     author_email='arnabchanda@gmail.com',
-    url='https://github.com/user/reponame',
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    url='https://github.com/Arnie09/DjangoSSHTunnelDatabaseConnector',
+    download_url='https://github.com/Arnie09/DjangoSSHTunnelDatabaseConnector/archive/refs/tags/0.1.tar.gz',
     keywords=['Django', 'sshtunnel', 'database'],
     install_requires=[
         'pymysql',
-        'logging',
         'sshtunnel'
     ],
     classifiers=[
